9e21d4b78487f17f710bb5f9d7b06a4557e82255
Vincent Servio <vincentservio@gmail.com>
2020-09-06 18:36:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+// Your code hfunf
+function saturdayFun (activity='roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activity='go to the office'){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (ne= '*') {
+    return function (adjective="special") {
+     return `You are ${ne}${adjective}${ne}!`
+}
+}
+
+let Calculator = {
+  add: function add(a,b){
+  return a+b
+  }
+  
+  subtract: function subtract(a,b){
+  return a-b
+  }
+}
